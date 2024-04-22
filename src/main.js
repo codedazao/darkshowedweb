b6b4fac8a6e038e11841911e706585fcca0e51c3
dazao <2995332268@qq.com>
2024-04-22 23:44:08 +0800

    初始化路由和首页,项目初始化的准备

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import "./style/global.css"
 import App from './App.vue'
+import router from './router/pageRouter'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/page/index/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        首页欢迎你
+    </div>
+</template>
+
+<style>
+</style>
